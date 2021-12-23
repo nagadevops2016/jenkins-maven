300417018490392bfb2771607eb6eb066efc65b7
nagadevops2016 <63189611+nagadevops2016@users.noreply.github.com>
2021-12-23 15:34:48 +0530

    Create s3.py

---
+++ sample/s3.py
@@ -0,0 +1,14 @@
+import boto3
+
+session = boto3.Session( 
+         aws_access_key_id='AKIAZIJX3N3NP62RM2FK', 
+         aws_secret_access_key='xPmZ6xgTK5XBK+W+a7OYE1kzNjpwkLjVDjdcZLXn')
+
+
+#Then use the session to get the resource
+s3 = session.resource('s3')
+
+my_bucket = s3.Bucket('*')
+
+for my_bucket_object in my_bucket.objects.all():
+    print(my_bucket_object.key)
