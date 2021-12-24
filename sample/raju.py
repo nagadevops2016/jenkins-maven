b51d3e232d0e7daccda8f9e11d032a2473b1f595
nagadevops2016 <63189611+nagadevops2016@users.noreply.github.com>
2021-12-24 14:49:32 +0530

    Update raju.py

--- sample/raju.py
+++ sample/raju.py
@@ -11,5 +11,15 @@ s3 = boto3.resource('s3', aws_access_key_id='AKIA6LQSYPWOAZDZTOMC',
 
 for bucket in s3.buckets.all():
     print (bucket.name)
-    print (bucket.size)
-  
+    
+allbuckets = s3resource.list_buckets()
+
+# Header Line for the output going to standard out
+print('Bucket'.ljust(45) + 'Size in Bytes'.rjust(25))
+for bucket in conn.get_all_buckets():
+	total_bytes = 0
+	name = bucket.name
+	for key in bucket:
+		total_bytes += key.size
+	total_bytes = total_bytes/1024/1024/1024
+	print ("Bucket Name:" ,name, "Size: ",total_bytes ,"GB")
