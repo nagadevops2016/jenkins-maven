b515fcf92ffbd0a46f4f7cf3a1ee2e67fb5bdf95
nagadevops2016 <63189611+nagadevops2016@users.noreply.github.com>
2021-12-24 14:20:35 +0530

    Update final.py

--- sample/final.py
+++ sample/final.py
@@ -16,3 +16,10 @@ allbuckets = s3client.list_buckets()
 
 # Header Line for the output going to standard out
 print('Bucket'.ljust(45) + 'Size in Bytes'.rjust(25))
+for bucket in conn.get_all_buckets():
+	total_bytes = 0
+	name = bucket.name
+	for key in bucket:
+		total_bytes += key.size
+	total_bytes = total_bytes/1024/1024/1024
+	print ("Bucket Name:" ,name, "Size: ",total_bytes ,"GB")
