5680f8e8d72dc459d13b71890ae75e96aa06a5df
nagadevops2016 <63189611+nagadevops2016@users.noreply.github.com>
2021-12-24 14:43:24 +0530

    Update raju.py

--- sample/raju.py
+++ sample/raju.py
@@ -2,10 +2,10 @@
 
 import logging
 import boto3
-from botocore.exceptions import ClientError
+
 
 # Retrieve the list of existing buckets
-s3 = boto3.client('s3', aws_access_key_id='AKIA6LQSYPWOAZDZTOMC',
+s3 = boto3.resource('s3', aws_access_key_id='AKIA6LQSYPWOAZDZTOMC',
  aws_secret_access_key='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')
 
 
