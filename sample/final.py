f0dc347d64cf5ef8556f62f755bb981c1c30c82e
nagadevops2016 <63189611+nagadevops2016@users.noreply.github.com>
2021-12-24 14:13:27 +0530

    Create final.py

---
+++ sample/final.py
@@ -0,0 +1,17 @@
+import boto
+import datetime
+import boto.s3.connection
+access_key = 'AKIA6LQSYPWOAZDZTOMC'
+secret_key = 'cGWLdJqrbtWMAzfgDygpgP34wdTxGKd9kXAUm68A'
+conn = boto.connect_s3( aws_access_key_id = access_key, aws_secret_access_key = secret_key)
+
+now = datetime.datetime.now()
+
+cw = boto3.client('cloudwatch')
+s3client = boto3.client('s3')
+
+# Get a list of all buckets
+allbuckets = s3client.list_buckets()
+
+# Header Line for the output going to standard out
+print('Bucket'.ljust(45) + 'Size in Bytes'.rjust(25))
